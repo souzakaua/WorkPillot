6c3d0e9f129eacfda31eb92d66c78dc6698b3e45
Kauã Souza <souza.kaua1405@gmail.com>
2025-06-10 23:48:00 -0300

    UP PROJECT

---
+++ src/main/java/com/project/workpillot/WorkpillotApplication.java
@@ -0,0 +1,13 @@
+package com.project.workpillot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkpillotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkpillotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/workpillot/WorkpillotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.workpillot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkpillotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
